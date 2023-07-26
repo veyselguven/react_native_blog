2d877c2d605a1ea9afd8fa2e4e4f89b1d8f8f70a
veyselguven <vgbasbaydar@gmail.com>
2023-07-26 10:27:38 -0700

    added blogpost to context

--- src/context/BlogContext.js
+++ src/context/BlogContext.js
@@ -1,11 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
 
 const BlogContext = React.createContext();
 
 export const BlogProvider = ({ children }) => {
-  const blogPost = [{ title: "Blog Post #1" }, { title: "Blog Post #2" }];
+  // const blogPost = [{ title: "Blog Post #1" }, { title: "Blog Post #2" }];
+  const [blogPost, setBlogPosts] = useState([]);
+
+  const addBlogPost = () => {
+    setBlogPosts([...blogPost, { title: `Blog Post #${blogPost.length + 1}` }]);
+  };
+
   return (
-    <BlogContext.Provider value={blogPost}>{children}</BlogContext.Provider>
+    <BlogContext.Provider value={{ data: blogPost, addBlogPost: addBlogPost }}>
+      {children}
+    </BlogContext.Provider>
   );
 };
 
